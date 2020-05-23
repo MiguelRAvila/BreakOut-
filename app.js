0957dc5afe51b9e0bbe3e9222084ea29ebf25c19
Miguel R. Ávila <miguelravila@outlook.com>
2020-05-22 23:22:43 -0500

    Bricks Improvement

--- app.js
+++ app.js
@@ -38,10 +38,12 @@ const brickInfo = {
 }
 
 const bricks [];
-for(let i =0; i <brickRowCount; i++) {
+for (let i = 0; i < brickRowCount; i++) {
     bricks[i] = [];
-    for(let j = 0; j < brickColumnCount; j++) {
+    for (let j = 0; j < brickColumnCount; j++) {
         const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = { x, y }
     }
 }
 
